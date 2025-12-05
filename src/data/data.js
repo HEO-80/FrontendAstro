174a072563e2f1c891ca48372cd136cceaacff58
Kardiad <mrakami98@gmail.com>
2025-12-05 15:58:18 +0100

    feature/tech-skills-jafet-pages

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+import Angular from "../assets/techskills/angular-svgrepo-com.svg"
+import Apache from "../assets/techskills/apache-svgrepo-com.svg"
+import Astro from '../assets/techskills/astro.svg'
+import Codeigniter from "../assets/techskills/codeigniter-logo-svgrepo-com.svg"
+import GitHub from '../assets/techskills/github-color-svgrepo-com.svg'
+import Javascript from "../assets/techskills/javascript-svgrepo-com.svg"
+import JQuery from "../assets/techskills/jquery-svgrepo-com.svg"
+import MySql from "../assets/techskills/mysql-svgrepo-com.svg"
+import Mariadb from "../assets/techskills/mariadb-svgrepo-com.svg"
+import PHP from "../assets/techskills/php-svgrepo-com.svg"
+import twig from "../assets/techskills/twig-svgrepo-com.svg"
+import typescript from "../assets/techskills/typescript-svgrepo-com.svg"
+
+export const jafetTechskills = [
+    Angular.src,
+    Apache.src,
+    Astro.src,
+    Codeigniter.src,
+    GitHub.src,
+    Javascript.src,
+    JQuery.src,
+    MySql.src,
+    Mariadb.src,
+    PHP.src,
+    twig.src,
+    typescript.src
+]
