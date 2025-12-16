3775571818797b496e3379b49710462076108c2d
NGwyndolin <nayade.g.marin@gmail.com>
2025-12-16 16:53:05 +0100

    update nayade- sin terminar pero quiero hacer pull no deberia dar fallo salu2

---
+++ src/data/nayade/gems.js
@@ -0,0 +1,25 @@
+export const gems = [
+  { id: 1, name: "Phosphophyllite" },
+  { id: 2, name: "Diamond" },
+  { id: 3, name: "Bort" },
+  { id: 4, name: "Cinnabar" },
+  { id: 5, name: "Antarcticite" },
+  { id: 6, name: "Jade" },
+  { id: 7, name: "Yellow Diamond" },
+  { id: 8, name: "Rutile" },
+  { id: 9, name: "Alexandrite" },
+  { id: 10, name: "Goshenite" },
+  { id: 11, name: "Euclase" },
+  { id: 12, name: "Hemimorphite" },
+  { id: 13, name: "Zircon" },
+  { id: 14, name: "Obsidian" },
+  { id: 15, name: "Peridot" },
+  { id: 16, name: "Benitoite" },
+  { id: 17, name: "Neptunite" },
+  { id: 18, name: "Morganite" },
+  { id: 19, name: "Red Beryl" },
+  { id: 20, name: "Aquamarine" },
+  { id: 21, name: "Padparadscha" }
+];
+
+export default gems;
