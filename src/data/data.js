09df55bda1d98ccc5a804b714699d332c51b984a
Kardiad <mrakami98@gmail.com>
2025-12-16 16:21:19 +0100

    feature/improving-jafet-site

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 import Angular from "../assets/techskills/angular-svgrepo-com.svg"
 import Apache from "../assets/techskills/apache-svgrepo-com.svg"
-import Astro from '../assets/techskills/astro.svg'
+import Astro from '../assets/techskills/astro-svgrepo-com.svg'
 import Codeigniter from "../assets/techskills/codeigniter-logo-svgrepo-com.svg"
 import GitHub from '../assets/techskills/github-color-svgrepo-com.svg'
 import Javascript from "../assets/techskills/javascript-svgrepo-com.svg"
@@ -10,6 +10,12 @@ import Mariadb from "../assets/techskills/mariadb-svgrepo-com.svg"
 import PHP from "../assets/techskills/php-svgrepo-com.svg"
 import twig from "../assets/techskills/twig-svgrepo-com.svg"
 import typescript from "../assets/techskills/typescript-svgrepo-com.svg"
+import Java from "../assets/techskills/java-svgrepo-com.svg"
+import Spring from "../assets/techskills/spring-svgrepo-com.svg"
+import Phalcon from "../assets/techskills/phalcon-svgrepo-com.svg"
+import Laravel from "../assets/techskills/laravel-svgrepo-com.svg"
+import Symfony from "../assets/techskills/symfony-svgrepo-com.svg"
+import Git from "../assets/techskills/git-svgrepo-com.svg"
 
 export const jafetTechskills = [
     Angular.src,
@@ -23,5 +29,29 @@ export const jafetTechskills = [
     Mariadb.src,
     PHP.src,
     twig.src,
-    typescript.src
+    Java.src,
+    typescript.src,
+    Spring.src,
+    Phalcon.src,
+    Symfony.src,
+    Laravel.src,
+    Git.src
+]
+
+export const jafetExperience = [
+    {
+        title : "Viva Marketing",
+        dates : "date",
+        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a magna leo. Duis ut accumsan ante. Proin venenatis tempus ipsum nec auctor. Maecenas laoreet nunc nec magna eleifend, sit amet rutrum nibh pulvinar. Donec fermentum gravida lectus, a molestie metus sodales quis. Ut in sollicitudin libero. Mauris maximus hendrerit libero at suscipit. Morbi at urna ut odio pharetra maximus id sed tortor. Mauris eu eros rutrum, lobortis ipsum vel, tristique orci."
+    },
+    {
+        title : "ADICAE",
+        dates : "",
+        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a magna leo. Duis ut accumsan ante. Proin venenatis tempus ipsum nec auctor. Maecenas laoreet nunc nec magna eleifend, sit amet rutrum nibh pulvinar. Donec fermentum gravida lectus, a molestie metus sodales quis. Ut in sollicitudin libero. Mauris maximus hendrerit libero at suscipit. Morbi at urna ut odio pharetra maximus id sed tortor. Mauris eu eros rutrum, lobortis ipsum vel, tristique orci."
+    },
+    {
+        title : "Embou",
+        dates : "",
+        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a magna leo. Duis ut accumsan ante. Proin venenatis tempus ipsum nec auctor. Maecenas laoreet nunc nec magna eleifend, sit amet rutrum nibh pulvinar. Donec fermentum gravida lectus, a molestie metus sodales quis. Ut in sollicitudin libero. Mauris maximus hendrerit libero at suscipit. Morbi at urna ut odio pharetra maximus id sed tortor. Mauris eu eros rutrum, lobortis ipsum vel, tristique orci."
+    }
 ]
